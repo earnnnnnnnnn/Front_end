129951b449c16cac7c65997a2b6ecdac6bb89365
eannnnnnnnn <s6606021631017@email.kmutnb.ac.th>
2025-03-03 17:45:32 +0700

    commit

---
+++ index.js
@@ -0,0 +1,94 @@
+//Description: Node.js HTML client
+//request: npm install express ejs axios body-parser
+
+const express = require('express');
+const axios = require('axios');
+const app = express();
+var bodyParser = require('body-parser');
+const path = require("path");
+
+//Base URL for the API
+//const base_url = "https://api.example.com";
+const base_url = "http://localhost:3000";
+
+//Set the template engine
+app.set("views" , path.join(__dirname, "/public/views"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+//Serve static files
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", async (req, res) => {
+    try{
+        const response = await axios.get(base_url + '/users');
+        res.render("Users/users", { users: response.data });
+    }catch(err){
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+// app.get("/book/:id", async (req, res) => {
+//     try{
+//         const response = await axios.get(base_url + '/books/' + req.params.id);
+//         res.render("book", { book: response.data });
+//     }catch(err){
+//         console.error(err);
+//         res.status(500).send('Error');
+//     }
+// });
+
+app.get("/create", (req, res) => {
+    res.render("Users/create");
+});
+
+app.post("/create", async (req, res) => {
+    try{
+        const data = {username: req.body.username, password:req.body.password, email: req.body.email, phone: req.body.phone};
+        
+        await axios.post(base_url + '/users', data);
+
+        res.redirect("/");
+    }catch(err){
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
+// app.get("/update/:id", async (req, res) => {
+//     try{
+//         const response = await axios.get(
+//         base_url + '/books/' + req.params.id);
+//         res.render("update", { book: response.data });
+//     }catch(err){
+//         console.error(err);
+//         res.status(500).send('Error');
+//     }
+// });
+
+// app.post("/update/:id", async (req, res) => {
+//     try{
+//         const data = {title: req.body.title, author: req.body.author };
+//         await axios.put(base_url + '/books/' + req.params.id, data);
+//         res.redirect("/");
+//     }catch(err){
+//         console.error(err);
+//         res.status(500).send('Error');
+//     }
+// });
+
+// app.get("/delete/:id", async (req, res) => {
+//     try{
+//         await axios.delete(base_url + '/books/' + req.params.id);
+//             res.redirect("/");
+//     }catch(err){
+//         console.error(err);
+//         res.status(500).send('Error');
+//     }
+// });
+
+app.listen(5000, () => {
+    console.log('Server started on http://localhost:5000');
+    });
