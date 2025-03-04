254ba4e8564421c5b72520233cf282da648bdfcd
fahsaii <fahsai.tpd@gmail>
2025-03-05 00:35:08 +0700

    ..

--- index.js
+++ index.js
@@ -100,10 +100,23 @@ app.get("/users/:id", async (req, res) => {
     }
 });
 
-
 app.get("/create", (req, res) => {
     res.render("create");
 });
+
+app.get("/paymentfrom", (req, res) => {
+    res.render("paymentfrom");
+});
+app.get("/users/:id", async (req, res) => {
+    try{
+        const response = await axios.get(base_url + '/users/' + req.params.id);
+        res.render("paymentfrom", { book: response.data });
+    }catch(err){
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
 
 // app.post("/create", async (req, res) => {
 //     try{
