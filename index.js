37953858572175d128a0019f443bcf27878b8081
eannnnnnnnn <s6606021631017@email.kmutnb.ac.th>
2025-03-11 23:43:19 +0700

    .

--- index.js
+++ index.js
@@ -380,16 +380,10 @@ app.get("/create", (req, res) => {
 });
 
 
-
-app.get("/users/:id", async (req, res) => {
-    try{
-        const response = await axios.get(base_url + '/users/' + req.params.id);
-        res.render("paymentfrom", { book: response.data });
-    }catch(err){
-        console.error(err);
-        res.status(500).send('Error');
-    }
+app.get("/order", (req, res) => {
+    res.render("order");
 });
+
 
 
 app.get("/rentalProcess", (req, res) => {
