7b6b6f3031207eb581d184d7165ac3e6f67766a2
eannnnnnnnn <s6606021631017@email.kmutnb.ac.th>
2025-03-13 04:26:57 +0700

    .

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", async (req, res) => {
