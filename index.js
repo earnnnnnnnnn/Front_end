4950d20abe486aed1f41b3fa09ffe72d1158859d
eannnnnnnnn <s6606021631017@email.kmutnb.ac.th>
2025-03-13 16:21:43 +0700

    ##

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(session({
     cookie: { secure: false }
 }));
 
-const base_url = "10.104.17.41:3000";
+//const base_url = "10.104.17.41:3000";
+const base_url = "210.246.202.121";
 // const base_url = 'https://node76398-project.proen.app.ruk-com.cloud';
 app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
